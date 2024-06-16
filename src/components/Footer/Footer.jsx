83d37177e4674e8a05d882615e336d8179424914
Sudeep <55885956+sudjos500@users.noreply.github.com>
2024-06-16 21:43:39 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer">© 2024 SCUBE, Thank You</div>;
+  return <div className="footer">© 2024 SCUBE - Sudeep Shetty S, Thank You</div>;
 };
 
 export default Footer;
